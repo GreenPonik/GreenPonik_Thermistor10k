4136a5d65740174ffc043c7f8148079c92c817b0
MkLHX <mickael.lehoux@gmail.com>
2020-07-23 19:30:28 +0200

    edit example

--- README.md
+++ README.md
@@ -55,13 +55,13 @@ def read_temp():
 import time
 from GreenPonik_thermistor_10k.GreenPonik_thermistor_10k import read_temp
 
+
 if __name__ == "__main__":
     while True:
         # read both celcius and fahrenheit temperatures
-        temperature = GreenPonik_thermistor_10k.read_temp()
+        temperature = .read_temp()
         print("celcius temp %.3f" % temperature)
         time.sleep(1)
-    pass
 
 ```
 
--- example/read_thermistor_10k_temperature.py
+++ example/read_thermistor_10k_temperature.py
@@ -1,10 +1,10 @@
 import time
-from GreenPonik_thermistor_10k import GreenPonik_thermistor_10k
+from GreenPonik_thermistor_10k.GreenPonik_thermistor_10k import read_temp
 
 
 if __name__ == "__main__":
     while True:
         # read both celcius and fahrenheit temperatures
-        temperature = GreenPonik_thermistor_10k.read_temp()
+        temperature = .read_temp()
         print("celcius temp %.3f" % temperature)
         time.sleep(1)
