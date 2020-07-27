d89b4b6041244067f8bee1457d4e254737523f61
MkLHX <mickael.lehoux@gmail.com>
2020-07-27 16:54:04 +0200

    fix typo

--- example/read_thermistor_10k_temperature.py
+++ example/read_thermistor_10k_temperature.py
@@ -5,6 +5,6 @@ from GreenPonik_thermistor_10k import read_temp
 if __name__ == "__main__":
     while True:
         # read both celcius and fahrenheit temperatures
-        temperature = .read_temp()
+        temperature = read_temp()
         print("celcius temp %.3f" % temperature)
         time.sleep(1)
