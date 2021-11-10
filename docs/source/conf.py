93eb19d23c361d36c63fba87ed745ab100f3b616
Mickael <mickael.lehoux@greenponik.com>
2021-11-10 12:30:46 +0100

    test press theme

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -31,7 +31,9 @@ release = '0.0.14'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = [
-    'sphinx_rtd_theme'
+    'sphinx-press-theme',
+    'sphinx_rtd_theme',
+    'recommonmark'
 ]
 
 # Add any paths that contain templates here, relative to this directory.
@@ -49,9 +51,12 @@ exclude_patterns = []
 # a list of builtin themes.
 #
 # html_theme = 'alabaster'
-html_theme = 'sphinx_rtd_theme'
+# html_theme = 'sphinx_rtd_theme'
+html_theme = 'press'
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = ['_static']
+
+source_suffix = ['.rst', '.md']
